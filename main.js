57a6587c4003e7b0d0e9236c3655bc5bf4c0985d
Eniola Osabiya <eosabiya@gmail.com>
2020-05-23 18:56:49 +0100

    Upload InventzBros post

---
+++ main.js
@@ -0,0 +1,28 @@
+$(".time_counter_init").TimeCircles({
+    "animation": "smooth",
+    "bg_width": 1.5,
+    "fg_width": 0.13333333333333333,
+    "circle_bg_color": "#354758",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#F7DE33",
+            "show": true
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#F7DE33",
+            "show": true
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#F7DE33",
+            "show": true
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#F7DE33",
+            "show": true
+        }
+    }
+});
